2ae54377e68fc20b3bcea516c6b101b13b689370
Jan Kretschmann <jankretschmann1@gmail.com>
2020-02-01 15:59:53 -0600

    first commit

---
+++ xls_to_csv.py
@@ -0,0 +1,25 @@
+import argparse
+import pandas as pd
+import os
+from pathlib import Path
+
+if __name__=='__main__':
+    ap = argparse.ArgumentParser()
+    ap.add_argument('-i', '--input', default='.',
+                    required=False, help='path to folder with excel files')
+    ap.add_argument('-o', '--output', default='./csv', required=False, help='where to save the csv files')
+    args = vars(ap.parse_args())
+
+    i_directory = os.fsencode(args['input'])
+
+    o_directory = os.fsencode(args['output'])
+    Path(args['output']).mkdir(exist_ok=True)
+
+
+    for file in os.listdir(i_directory):
+        filename = os.fsdecode(file)
+        if filename.endswith(".xls") or filename.endswith(".xlsx"): 
+            data = pd.read_excel(args['input']+'/'+filename, index_col=0)
+            with open(args['output']+'/'+filename[:-filename.find('.xls')+1] + '.csv', 'w') as csvfile:
+                data.to_csv(csvfile)
+        
